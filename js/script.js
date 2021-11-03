308a96057db0bf277acef7ee50cce65d83b2110c
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-03 20:36:55 -0300

    [Exercicio extra + mostrando resoltado do IMC]

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-var titulo = document.querySelector(".titulo")
-titulo.textContent = "Site de Nutrição"
+var paciente = document.querySelector("#primeiro-paciente");
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+var pacienteNome = paciente.querySelector(".info-nome").textContent;
+
+
+console.log(paciente);
+console.log(peso);
+console.log(pacienteNome);
+console.log(altura);
+
+var imc = peso / (altura * altura);
+
+paciente.querySelector(".info-imc").textContent = imc
+
+console.log(imc)

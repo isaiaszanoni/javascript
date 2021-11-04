520360ddb9d1dd19083176b90b5f5c37d12a401e
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-04 17:00:04 -0300

    [Alterando estilos por meio do Javascript - demarcando valores invalidos]

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-
-
 var pacientes = document.querySelectorAll(".paciente")
-
-console.log(pacientes)
 
 for (var i = 0; i < pacientes.length; i++) {
     var paciente = i;
@@ -14,11 +10,6 @@ for (var i = 0; i < pacientes.length; i++) {
     var pacienteNome = paciente.querySelector(".info-nome").textContent;
 
 
-    console.log(paciente);
-    console.log(peso);
-    console.log(pacienteNome);
-    console.log(altura);
-
     var imc = peso / (altura * altura);
 
     var alturaValida = true;
@@ -28,18 +19,20 @@ for (var i = 0; i < pacientes.length; i++) {
     if (peso <= 0 || peso >= 250) {
         pesoValido = false;
         mensagem += "Peso inválido! ";
-        //paciente.querySelector(".info-imc").textContent = "Peso inválido!"
+        paciente.querySelector(".info-peso").classList.add("paciente-invalido");     //paciente.querySelector(".info-imc").textContent = "Peso inválido!"
     }
 
     if (altura <= 0 || altura >= 2.5) {
         alturaValida = false;
         mensagem += "Altura inválida! "
+        paciente.querySelector(".info-altura").classList.add("paciente-invalido");
     }
 
     if (pesoValido && alturaValida) {
-        paciente.querySelector(".info-imc").textContent = imc;
+        paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
     } else {
         paciente.querySelector(".info-imc").textContent = mensagem
     }
 }
 console.log("cheguei ao fim")
+

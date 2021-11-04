534cad156449038077e2e7c139662087e45f9369
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-04 06:34:55 -0300

    [Validacao de altura e peso]

--- js/script.js
+++ js/script.js
@@ -11,6 +11,28 @@ console.log(altura);
 
 var imc = peso / (altura * altura);
 
+var alturaValida = true;
+var pesoValido = true;
+var mensagem = ""
+
+if (peso <= 0 || peso >= 250){
+    pesoValido = false;
+    mensagem += "Peso inválido! ";
+    //paciente.querySelector(".info-imc").textContent = "Peso inválido!"
+}
+
+if (altura <=0 || altura >= 2.5) {
+    alturaValida = false;
+    mensagem += "Altura inválida! "
+}
+
+if (pesoValido && alturaValida) {
+    paciente.querySelector(".info-imc").textContent = imc;
+} else {
+    paciente.querySelector(".info-imc").textContent = mensagem;
+}
+
+/*
 paciente.querySelector(".info-imc").textContent = imc
 
-console.log(imc)
+console.log(imc)*/

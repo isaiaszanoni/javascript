c16f6fb37be61539b598e8ecb794ccd2001ce499
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-04 07:27:27 -0300

    [Esboço - codigo incompleto/com erro - Loop for para calcular todos os pacientes]

--- js/script.js
+++ js/script.js
@@ -1,37 +1,46 @@
 var paciente = document.querySelector("#primeiro-paciente");
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
-var pacienteNome = paciente.querySelector(".info-nome").textContent;
+
+var pacientes = document.querySelectorAll(".paciente")
+
+console.log(pacientes)
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = paciente[i];
+
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var pacienteNome = paciente.querySelector(".info-nome").textContent;
 
 
-console.log(paciente);
-console.log(peso);
-console.log(pacienteNome);
-console.log(altura);
+    console.log(paciente);
+    console.log(peso);
+    console.log(pacienteNome);
+    console.log(altura);
 
-var imc = peso / (altura * altura);
+    var imc = peso / (altura * altura);
 
-var alturaValida = true;
-var pesoValido = true;
-var mensagem = ""
+    var alturaValida = true;
+    var pesoValido = true;
+    var mensagem = ""
 
-if (peso <= 0 || peso >= 250){
-    pesoValido = false;
-    mensagem += "Peso inválido! ";
-    //paciente.querySelector(".info-imc").textContent = "Peso inválido!"
+    if (peso <= 0 || peso >= 250) {
+        pesoValido = false;
+        mensagem += "Peso inválido! ";
+        //paciente.querySelector(".info-imc").textContent = "Peso inválido!"
+    }
+
+    if (altura <= 0 || altura >= 2.5) {
+        alturaValida = false;
+        mensagem += "Altura inválida! "
+    }
+
+    if (pesoValido && alturaValida) {
+        paciente.querySelector(".info-imc").textContent = imc;
+    } /*else {
+        paciente.querySelector(".info-imc").textContent = mensagem;
+    }*/
 }
-
-if (altura <=0 || altura >= 2.5) {
-    alturaValida = false;
-    mensagem += "Altura inválida! "
-}
-
-if (pesoValido && alturaValida) {
-    paciente.querySelector(".info-imc").textContent = imc;
-} else {
-    paciente.querySelector(".info-imc").textContent = mensagem;
-}
-
+console.log("cheguei ao fim")
 /*
 paciente.querySelector(".info-imc").textContent = imc
 

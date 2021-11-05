833c16d6695d028dbb6d8ee23c20bf2bb09c8f92
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-04 22:03:34 -0300

    [Adicionando novos pacientes à tabela]

--- js/script.js
+++ js/script.js
@@ -35,17 +35,13 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
-
-
 function mostraMensagem(){
     console.log("Olha, um click! *_*")
 }
 
-// var titulo = document.querySelector("h1");
 document.querySelector(".info-nome").addEventListener("click", mostraMensagem);
 
 /* ! Função anônima ! */
-
 document.querySelector("h1").addEventListener("click", function(){
     console.log((document.querySelector("h1").textContent) + " por função anônima!")
 });
@@ -54,5 +50,36 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
-    console.log("Oi! Botão está funcionando!")
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes")
+
+    tabela.appendChild(pacienteTr);
+
+
 });
+

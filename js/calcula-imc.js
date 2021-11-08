d18d25f2a242721b677e226d2829906ef0bf3587
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-08 12:12:11 -0300

    [Funcoes para criar tabela e pegar dados do formulario]

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -36,10 +36,11 @@ for (var i = 0; i < pacientes.length; i++) {
 
 
 function calculaImc(peso, altura){
+   
     var imc = 0;
-
-    imc = peso / (altura * altura).toFixed(2);
-
-    return imc;
+    imc = peso / (altura * altura);
+    
+    return imc.toFixed(2);
+    
 }
 
